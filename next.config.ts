49e233c4c69b1faa34388ef549edf7817bac0ba5
MnLsVt <manolis277@gmail.com>
2026-02-01 19:14:05 +0100

    fixed deployment issues

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,19 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'export'
 };
+
+// module.exports = {
+//   images: {
+//     remotePatterns: [
+//       {
+//         protocol: "https",
+//         hostname: "raw.githubusercontent.com",
+//         pathname: "/Mnlsvt/**",
+//       },
+//     ],
+//   },
+// };
 
 export default nextConfig;
