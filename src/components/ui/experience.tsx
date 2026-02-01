2c2c002e04a77dca1f358ca00a1119f9d2d3f885
MnLsVt <manolis277@gmail.com>
2026-02-01 20:41:18 +0100

    fixed a typo

--- src/components/ui/experience.tsx
+++ src/components/ui/experience.tsx
@@ -52,7 +52,7 @@ export default function Experience() {
         }}
       >
         <Tab label="Uppsala Uni" />
-        <Tab label="Theagenio" />
+        <Tab label="Theageneio" />
       </Tabs>
 
       {/* RIGHT CONTENT */}
